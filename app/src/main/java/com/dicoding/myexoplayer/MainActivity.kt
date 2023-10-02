778716aa1917d704b1b854ae0706b8ea4c01dca4
ArtForYou <kakakunyukya@gmail.com>
2023-10-02 22:31:25 +0700

    Update Foreground service for play in background

--- app/src/main/java/com/dicoding/myexoplayer/MainActivity.kt
+++ app/src/main/java/com/dicoding/myexoplayer/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.dicoding.myexoplayer
 
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowInsetsController
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
 import com.dicoding.myexoplayer.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.MoreExecutors
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +21,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         hideSystemUi()
-
-        val videoItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
-        val audioItem = MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
-        val player = ExoPlayer.Builder(this).build().also {exoPlayer ->
-            exoPlayer.setMediaItem(videoItem)
-            exoPlayer.addMediaItem(audioItem)
-            exoPlayer.prepare()
-        }
-        binding.playerView.player = player
     }
 
     private fun hideSystemUi() {
@@ -37,5 +29,15 @@ class MainActivity : AppCompatActivity() {
             controller.hide(WindowInsetsCompat.Type.systemBars())
             controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
+        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+        controllerFuture.addListener(
+            { binding.playerView.player = controllerFuture.get() },
+            MoreExecutors.directExecutor()
+        )
     }
 }
